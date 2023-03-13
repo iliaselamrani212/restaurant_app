9ef3f99b2311ab17d9edff56c4e72a17337a15ee
younes20044 <youn.elamrani@edu.umi.ac.ma>
2023-03-13 03:00:18 +0100

    gggj

--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,8 @@
 <?php
 use App\Http\Controllers\FoodController;
 use Illuminate\Support\Facades\Route;
-use Illuminate\Http\Request;
-use App\Models\Visitor ;
 
 
-<<<<<<< HEAD
 
 
 
@@ -19,15 +16,3 @@ Route::get('food',[
     Route::get('registration', 'App\Http\Controllers\AuthController@registration');
     Route::post('post-registration', 'App\Http\Controllers\AuthController@postRegistration'); 
     Route::get('logout', 'App\Http\Controllers\AuthController@logout');
-=======
-Route::get('/', function (Request $request) {
-    $visitor = new Visitor();
-$visitor->ip_address = $request->ip();
-$visitor->visit_date = now()->format('Y-m-d');
-$visitor->save();
-    return view('welcome');
-});
-
-Route::get('users', 'App\Http\Controllers\UserController@getUsers')->name("users");
-Route::get('dashboard', 'App\Http\Controllers\UserController@dashboard')->name("dashboard");
->>>>>>> 9f300c888fd4533428f23f0ee99206fc13435fcf
